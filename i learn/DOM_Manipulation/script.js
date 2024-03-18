5acef872e47447eebab10fae53736eaf4c41a9c7
zahyaah <itszayd2003@gmail.com>
2024-03-18 20:40:04 +0530

    add element 2

--- i learn/DOM_Manipulation/script.js
+++ i learn/DOM_Manipulation/script.js
@@ -7,3 +7,11 @@ body.append(div);
 const span = document.createElement("span")
 span.innerText = "Lorem ipsum dolor sit";
 body.append(span);
+body.append(document.createElement("br"));
+
+
+const anchor = document.createElement("a");
+anchor.setAttribute("href", "https://google.com");
+anchor.setAttribute("target", "_blank");
+anchor.innerText = "Googled!";
+body.append(anchor);
