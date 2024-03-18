5ce49f3676d8fcfbe0181fb266bdf4795513f7bf
zahyaah <itszayd2003@gmail.com>
2024-03-18 23:51:09 +0530

    add data attribute

--- i learn/DOM_Manipulation/script.js
+++ i learn/DOM_Manipulation/script.js
@@ -17,4 +17,12 @@ anchor.setAttribute("target", "_blank");
 anchor.innerText = "Googled!";
 body.append(anchor);
 
-div.remove();
+// const img = document.createElement("img");
+// img.alt = "zayd";
+// img.src = "https://img.freepik.com/free-vector/programming-concept-illustration_114360-1351.jpg?w=1380&t=st=1710785682~exp=1710786282~hmac=e155ee461076ce51ab67c367919b9e9f0ecb575c048ee04c87f839f7e5e4e94a";
+// img.height = 200;
+// img.width = 250;
+// body.append(img);
+
+span.dataset.newData = "cool";
+// console.log(span.dataset);
