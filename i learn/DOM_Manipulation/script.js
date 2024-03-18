3cc59d0fdd32d4ff81e99b9a6c883391dc520f5c
zahyaah <itszayd2003@gmail.com>
2024-03-18 20:36:02 +0530

    add element 1

---
+++ i learn/DOM_Manipulation/script.js
@@ -0,0 +1,9 @@
+const body = document.body;
+
+const div = document.createElement("div");
+div.innerText = "Created";
+body.append(div);
+
+const span = document.createElement("span")
+span.innerText = "Lorem ipsum dolor sit";
+body.append(span);
